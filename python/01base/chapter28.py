63ff2e32085890f61a3d02a617be87c489c5fd4a
code1990 <s1332177151@sina.com>
2020-02-14 22:59:00 +0800

    init

--- python/01base/chapter28.py
+++ python/01base/chapter28.py
@@ -2,8 +2,8 @@
 import os
 # path = "D:\\\\github\\\\bootPython\\\\python\\\\02high\\\\"
 path = "C:\\Users\\Administrator\\Desktop\\test\\"
-for i in range(10,100):
-    name = path+"exercise"+str(i)+".py"
+for i in range(2,10):
+    name = path+"selenium0"+str(i)+".py"
     print(name)
     # open方法可以创建一个文件
     file = open(name,"w")
---
+++ python/04selenium/selenium01.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+
+# pip install selenium
+# 一定要指明 executable_path='C:\driver\chromedriver.exe'
+# 否则无法启动
+driver = webdriver.Chrome(executable_path='C:\driver\chromedriver.exe')
+
+driver.get('https://www.baidu.com')
+
+print(driver.title)
+
+driver.quit()
