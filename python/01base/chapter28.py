8b9978dfc34a0bafb4300daf35cd5d77888285a4
code1990 <s1332177151@sina.com>
2020-02-15 20:08:44 +0800

    init

--- python/01base/chapter28.py
+++ python/01base/chapter28.py
@@ -2,8 +2,8 @@
 import os
 # path = "D:\\\\github\\\\bootPython\\\\python\\\\02high\\\\"
 path = "C:\\Users\\Administrator\\Desktop\\test\\"
-for i in range(10,21):
-    name = path+"selenium0"+str(i)+".py"
+for i in range(10,77):
+    name = path+"demo0"+str(i)+".py"
     print(name)
     # open方法可以创建一个文件
     file = open(name,"w")
--- python/01base/chapter29.py
+++ python/01base/chapter29.py
@@ -1,16 +1,25 @@
 from selenium import webdriver
-driver = webdriver.Chrome(executable_path='C:\driver\chromedriver.exe')
-
-driver.get('https://www.runoob.com/python/python-100-examples.html')
-
-content = driver.find_element_by_id("content")
-
-links = content.find_elements_by_tag_name("a")
-print(type(links))
-print(len(links))
-for i in range(50,len(links)):
-    links[i].click()
-
-# print(driver.title)
-
+import time
+file = open("C:\\Users\\Administrator\\Desktop\\122.txt","r+")
+for line in file.readlines():
+    driver = webdriver.Chrome(executable_path='C:\driver\chromedriver.exe')
+    driver.get(line)
+    print(line,end="")
+    time.sleep(2)
+# while file.r
+#
+#
+#
+#
+# content = driver.find_element_by_id("content")
+#
+# links = content.find_elements_by_tag_name("a")
+# print(type(links))
+# print(len(links))
+# for link in links:
+#     print(link.get_attribute("href"))
+#     # link.click()
+#
+# # print(driver.title)
+#
 # driver.quit()
