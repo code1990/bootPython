3266a6fe817e8c27b68fa8bac3303c891718664d
code1990 <s1332177151@sina.com>
2020-03-29 22:53:08 +0800

    init

--- python/10pyautogui/screenshot.py
+++ python/10pyautogui/screenshot.py
@@ -3,11 +3,14 @@ import pyautogui
 im1 = pyautogui.screenshot()
 im1.save('my_screenshot.png')
 
-im2 = pyautogui.screenshot('my_screenshot2.png')
+# im2 = pyautogui.screenshot('my_screenshot2.png')
 # 屏幕查找图片位置并获取中间点
 #在当前屏幕中查找指定图片(图片需要由系统截图功能截取的图)
-coords = pyautogui.locateOnScreen('folder.png')
+# coords = pyautogui.locateOnScreen('folder.png')
 #获取定位到的图中间点坐标
-x,y=pyautogui.center(coords)
+# x,y=pyautogui.center(coords)
 #右击该坐标点
-pyautogui.rightClick(x,y)
+# pyautogui.rightClick(x,y)
+# pyautogui 可以传递区域的左、顶、宽和高的四个整数元组来捕获图片
+# 图片的起点x 图片的起点y 图片宽 图片高
+pic_3 = pyautogui.screenshot("my_screenshot.png",region=(1204,739, 50, 30))
--- python/11pywinauto/test4.py
+++ python/11pywinauto/test4.py
@@ -1,7 +1,10 @@
 import pywinauto
 import pyautogui
 import time
-
+# 本地先安装360浏览器 安装领跑插件 并且登录完成 可以保证后面可以免密登录
+# 本地运行 请设置360浏览器安装位置
+# 配置 配置https://www.linkedin.com/feed/为360主页
+# 设置 人脉搜索关键字的位置
 # 1.运行360浏览器
 browser_path = 'D:\\360\\360se6\\Application\\360se.exe'
 kw_path = 'C:\\Users\\admin\\Desktop\\searchkw.txt'
