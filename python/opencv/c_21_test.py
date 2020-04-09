5175da960d8eb837c2b2b4b7671042f0c6713026
code1990 <s1332177151@sina.com>
2020-04-09 15:43:21 +0800

    init

--- python/opencv/c_21_test.py
+++ python/opencv/c_21_test.py
@@ -44,6 +44,7 @@ class c_21_test(unittest.TestCase):
 
 	# 21.1.3轮廓的近似方法
 	# 这就是cv2.CHAIN_APPROX_SIMPLE 要做的。它会将轮廓上的冗余点都去掉，压缩轮廓，从而节省内存开支。
+
 	def test_212(self):
 		# 21.2轮廓特征
 		print("")
@@ -118,7 +119,7 @@ class c_21_test(unittest.TestCase):
 		# 21.2.9椭圆拟合
 		# 使用的函数为cv2.ellipse()，返回值其实就是旋转边界矩形的内切圆。
 		ellipse = cv2.fitEllipse(cnt)
-		im = cv2.ellipse(img,ellipse,(0,255,0),2)
+		im = cv2.ellipse(img, ellipse, (0, 255, 0), 2)
 		# 21.2.10线拟合
 		# 根据一组点拟合出一条直线
 		rows, cols = img.shape[:2]
@@ -126,30 +127,129 @@ class c_21_test(unittest.TestCase):
 		lefty = int((-x * vy / vx) + y)
 		righty = int(((cols - x) * vy / vx) + y)
 		img = cv2.line(img, (cols - 1, righty), (0, lefty), (0, 255, 0), 2)
-	def test_213(self):
+		# def test_213(self):
 		# 21.3轮廓的性质
+		# 可以在Matlab# regionprops documentation 更多的图像特征。
 		print("")
-
 		# 21.3.1长宽比
+		# 边界矩形的宽高比
+		x, y, w, h = cv2.boundingRect(cnt)
+		aspect_ratio = float(w) / h
+		print(aspect_ratio)
 		# 21.3.2Extent
+		# 轮廓面积与边界矩形面积的比。
+		area = cv2.contourArea(cnt)
+		x, y, w, h = cv2.boundingRect(cnt)
+		rect_area = w * h
+		extent = float(area) / rect_area
+		print(extent)
 		# 21.3.3Solidity
+		# 轮廓面积与凸包面积的比。
+		area = cv2.contourArea(cnt)
+		hull = cv2.convexHull(cnt)
+		hull_area = cv2.contourArea(hull)
+		solidity = float(area) / hull_area
 		# 21.3.4EquivalentDiameter
+		# 与轮廓面积相等的圆形的直径
+		area = cv2.contourArea(cnt)
+		equi_diameter = np.sqrt(4 * area / np.pi)
+		print(equi_diameter)
 		# 21.3.5方向
+		# 对象的方向，下面的方法还会返回长轴和短轴的长度
+		(x, y), (MA, ma), angle = cv2.fitEllipse(cnt)
 		# 21.3.6掩模和像素点
+		# 有时我们需要构成对象的所有像素点，我们可以这样做：
+		mask = np.zeros(img.shape, np.uint8)
+		# 这里一定要使用参数-1, 绘制填充的的轮廓
+		cv2.drawContours(mask, [cnt], 0, 255, -1)
+		pixelpoints = np.transpose(np.nonzero(mask))
+		# pixelpoints = cv2.findNonZero(mask)
 		# 21.3.7最大值和最小值及它们的位置
+		# 使用掩模图像得到这些参数。
+		min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(img, mask=mask)
 		# 21.3.8平均颜色及平均灰度
+		# 使用相同的掩模求一个对象的平均颜色或平均灰度
+		mean_val = cv2.mean(im, mask=mask)
 		# 21.3.9极点
-	def test_214(self):
+		# 一个对象最上面，最下面，最左边，最右边的点。
+		leftmost = tuple(cnt[cnt[:, :, 0].argmin()][0])
+		rightmost = tuple(cnt[cnt[:, :, 0].argmax()][0])
+		topmost = tuple(cnt[cnt[:, :, 1].argmin()][0])
+		bottommost = tuple(cnt[cnt[:, :, 1].argmax()][0])
+		# def test_214(self):
 		# 21.4轮廓：更多函数
-		print("")
+		# 凸缺陷，以及如何找凸缺陷
+		# • 找某一点到一个多边形的最短距离
+		# • 不同形状的匹配
+		# 对象上的任何凹陷都被成为凸缺陷。
+		# OpenCV 中有一个函数cv.convexityDefect() 可以帮助我们找到凸缺陷。
+		# returnPoints 一定要是False。
+		hull = cv2.convexHull(cnt, returnPoints=False)
+		defects = cv2.convexityDefects(cnt, hull)
 
+	# 它会返回一个数组，其中每一行包含的值是[起点，终点，最远的点，到最
+	# 远点的近似距离]
+	def test_2211(self):
+		print("")
+		img = cv2.imread('python.png')
+		img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+		ret, thresh = cv2.threshold(img_gray, 127, 255, 0)
+		contours, hierarchy = cv2.findContours(thresh, 2, 1)
+		cnt = contours[0]
+		hull = cv2.convexHull(cnt, returnPoints=False)
+		defects = cv2.convexityDefects(cnt, hull)
+		for i in range(defects.shape[0]):
+			s, e, f, d = defects[i, 0]
+		start = tuple(cnt[s][0])
+		end = tuple(cnt[e][0])
+		far = tuple(cnt[f][0])
+		cv2.line(img, start, end, [0, 255, 0], 2)
+		cv2.circle(img, far, 5, [0, 0, 255], -1)
+		cv2.imshow('img', img)
+		cv2.waitKey(0)
+		cv2.destroyAllWindows()
 		# 21.4.1凸缺陷
 		# 21.4.2PointPolygonTest
+		# 求解图像中的一个点到一个对象轮廓的最短距离。如果点在轮廓的外部，
+		# 返回值为负。如果在轮廓上，返回值为0。如果在轮廓内部，返回值为正。
+		# 下面我们以点（50，50）为例：
+		# 此函数的第三个参数是measureDist。如果设置为True，就会计算最
+		# 短距离。如果是False，只会判断这个点与轮廓之间的位置关系（返回值为
+		# +1，-1，0）
+		dist = cv2.pointPolygonTest(cnt, (50, 50), True)
+
+	def test_220(self):
 		# 21.4.3形状匹配
+		# 	函数cv2.matchShape() 可以帮我们比较两个形状或轮廓的相似度。如
+		# 果返回值越小，匹配越好。它是根据Hu 矩来计算的
+		img1 = cv2.imread('star.jpg', 0)
+		img2 = cv2.imread('star2.jpg', 0)
+		ret, thresh = cv2.threshold(img1, 127, 255, 0)
+		ret, thresh2 = cv2.threshold(img2, 127, 255, 0)
+		contours, hierarchy = cv2.findContours(thresh, 2, 1)
+		cnt1 = contours[0]
+		contours, hierarchy = cv2.findContours(thresh2, 2, 1)
+		cnt2 = contours[0]
+		ret = cv2.matchShapes(cnt1, cnt2, 1, 0.0)
+		print(ret)
+
 	def test_215(self):
 		# 21.5轮廓的层次结构
+		# 现在我们要学习轮廓的层次结构了，比如轮廓之间的父子关系。
 		print("")
-
 		# 21.5.1什么是层次结构
 		# 21.5.2OpenCV中层次结构
 		# 21.5.3轮廓检索模式
+		# 可以确定一个轮廓与其他轮廓是怎样连接的，
+		# 比如它是不是某个轮廓的子轮廓，或者是父轮廓。这种关系就成为组织结构
+		# 了解释层次结构，外轮廓，子轮廓，父轮廓，子轮廓。
+		# OpenCV 使用一个含有四个元素的数组表示。[Next，Previous，First_Child，Parent]
+		# Next 表示同一级组织结构中的下一个轮廓。
+		# Previous 表示同一级结构中的前一个轮廓。
+		# First_Child 表示它的第一个子轮廓。
+		# Parent 表示它的父轮廓。
+		# 21.5.3 轮廓检索模式
+		# RETR_LIST 它只是提取所有的轮廓
+		# RETR_EXTERNAL 返回最外边的的轮廓，所有的子轮廓都会被忽略掉。
+		# RETR_CCOMP 返回所有的轮廓并将轮廓分为两级组织结构
+		# RETR_TREE 返回所有轮廓，并且创建一个完整的组织结构列表
--- python/opencv/c_22_test.py
+++ python/opencv/c_22_test.py
@@ -1,30 +1,117 @@
 import unittest
+import numpy as np
+from matplotlib import pyplot as plt
+import cv2
 
+
+# 使用OpenCV 或Numpy 函数计算直方图
+# • 使用Opencv 或者Matplotlib 函数绘制直方图
+# • 将要学习的函数有：cv2.calcHist()，np.histogram()
 class c_22_test(unittest.TestCase):
 	def test_221(self):
-		#22.1直方图的计算，绘制与分析
-		print("")
+		# 22.1直方图的计算，绘制与分析
+		# 通过直方图你可以对整幅图像的灰度分布有一个整体的了解
+		# 直方图其实就是对图像的另一种解释
+		# 直方图是根据灰度图像绘制的，而不是彩色图像
+		# BINS：上面的直方图显示了每个灰度值对应的像素数
+		# DIMS：表示我们收集数据的参数数目
+		# RANGE：就是要统计的灰度值范围
+		#
+		# 函数cv2.calcHist 可以帮助我们统计一幅图像的直方图。
+		# cv2:calcHist(images; channels; mask; histSize; ranges[; hist[; accumulate]])
+		# 1. images: 原图像（图像格式为uint8 或float32）。
+		# 当传入函数时应该用中括号[] 括起来，例如：[img]。
+		# 2. channels: 同样需要用中括号括起来，它会告诉函数我们要统计那幅图像的直方图。
+		# 如果输入图像是灰度图，它的值就是[0]；
+		# 如果是彩色图像的话，传入的参数可以是[0]，[1]，[2] 它们分别对应着通道B，G，R。
+		# 3. mask: 掩模图像。要统计整幅图像的直方图就把它设为None。
+		# 但是如果你想统计图像某一部分的直方图的话，你就需要制作一个掩模图像
+		# 4. histSize:BIN 的数目。也应该用中括号括起来，例如：[256]。
+		# 5. ranges: 像素值范围，通常为[0，256]
+		img = cv2.imread('python.png', 0)
+		# 别忘了中括号[img],[0],None,[256],[0,256]，只有mask 没有中括号
+		hist = cv2.calcHist([img], [0], None, [256], [0, 256])
+		# hist 是一个256x1 的数组，每一个值代表了与次灰度值对应的像素点数目。
+		# img.ravel() 将图像转成一维数组，这里没有中括号。
+		hist, bins = np.histogram(img.ravel(), 256, [0, 256])
 
-		#22.1.1统计直方图
-		#22.1.2绘制直方图
-		#22.1.3使用掩模
+	# 对于一维直方图， 我们最好使用这个函数
+	# hist=np.bincount(img.ravel()，minlength=256)
+	# 22.1.1统计直方图
+	# 22.1.2绘制直方图
+	def test_1111(self):
+		# 1. Short Way（简单方法）：使用Matplotlib 中的绘图函数。
+		img = cv2.imread('python.png', 0)
+		plt.hist(img.ravel(), 256, [0, 256])
+		plt.show()
+
+	def test_1112(self):
+		# 2. Long Way（复杂方法）：使用OpenCV 绘图函数
+		img = cv2.imread('python.png')
+		color = ('b', 'g', 'r')
+		# 对一个列表或数组既要遍历索引又要遍历元素时
+		# 使用内置enumerrate 函数会有更加直接，优美的做法
+		# enumerate 会将数组或列表组成一个索引序列。
+		# 使我们再获取索引和索引内容的时候更加方便
+		for i, col in enumerate(color):
+			histr = cv2.calcHist([img], [i], None, [256], [0, 256])
+		plt.plot(histr, color=col)
+		plt.xlim([0, 256])
+		plt.show()
+
+	def test_1113(self):
+		# 22.1.3使用掩模
+		# 要统计图像某个局部区域的直方图只需要构建一副掩模图像。将要统计的
+		# 部分设置成白色，其余部分为黑色，就构成了一副掩模图像。然后把这个掩模
+		# 图像传给函数就可以了。
+		img = cv2.imread('python.png', 0)
+		# create a mask
+		mask = np.zeros(img.shape[:2], np.uint8)
+		mask[100:300, 100:400] = 255
+		masked_img = cv2.bitwise_and(img, img, mask=mask)
+		# Calculate histogram with mask and without mask
+		# Check third argument for mask
+		hist_full = cv2.calcHist([img], [0], None, [256], [0, 256])
+		hist_mask = cv2.calcHist([img], [0], mask, [256], [0, 256])
+		plt.subplot(221), plt.imshow(img, 'gray')
+		plt.subplot(222), plt.imshow(mask, 'gray')
+		plt.subplot(223), plt.imshow(masked_img, 'gray')
+		plt.subplot(224), plt.plot(hist_full), plt.plot(hist_mask)
+		plt.xlim([0, 256])
+		plt.show()
+
 	def test_222(self):
-		#22.2直方图均衡化
-		print("")
+		# 22.2直方图均衡化
+		# 如何使用它来改善图片的对比。
+		# 把它的直方图做一个横向拉伸（如下图），这就是直方图均衡化要做的事情
+		# 这种操作会改善图像的对比度。
+		# 先看看怎样使用Numpy 来进行直方图均衡化，
+		# 然后再学习使用OpenCV 进行直方图均衡化。
+		img = cv2.imread('python.png', 0)
+		# flatten() 将数组变成一维
+		hist, bins = np.histogram(img.flatten(), 256, [0, 256])
+		# 计算累积分布图
+		cdf = hist.cumsum()
+		cdf_normalized = cdf * hist.max() / cdf.max()
+		plt.plot(cdf_normalized, color='b')
+		plt.hist(img.flatten(), 256, [0, 256], color='r')
+		plt.xlim([0, 256])
+		plt.legend(('cdf', 'histogram'), loc='upper left')
+		plt.show()
 
-		#22.2.1OpenCV中的直方图均衡化
-		#22.2.2CLAHE有限对比适应性直方图均衡化
+	# 22.2.1OpenCV中的直方图均衡化
+	# 22.2.2CLAHE有限对比适应性直方图均衡化
 	def test_2232(self):
-		#22.32D直方图
+		# 22.32D直方图
 		print("")
 
-		#22.3.1介绍
-		#22.3.2OpenCV中的2D直方图
-		#22.3.3Numpy中2D直方图
-		#22.3.4绘制2D直方图
+	# 22.3.1介绍
+	# 22.3.2OpenCV中的2D直方图
+	# 22.3.3Numpy中2D直方图
+	# 22.3.4绘制2D直方图
 	def test_224(self):
-		#22.4直方图反向投影
+		# 22.4直方图反向投影
 		print("")
 
-		#22.4.1Numpy中的算法
-		#22.4.2OpenCV中的反向投影
+# 22.4.1Numpy中的算法
+# 22.4.2OpenCV中的反向投影
