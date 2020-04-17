dc7d1d10ca432f6c0e8e49e5f0c0e05dc8d56964
code1990 <s1332177151@sina.com>
2020-04-17 14:01:31 +0800

    init

--- python/opencv/c_50_test.py
+++ python/opencv/c_50_test.py
@@ -1,8 +1,17 @@
 import unittest
-
+import numpy as np
+import cv2
+# 使用修补技术去除老照片中小的噪音和划痕
+# • 使用OpenCV 中与修补技术相关的函数
 class c_50_test(unittest.TestCase):
 	def test_501(self):
 		#50.1基础
+		img = cv2.imread('messi_2.jpg')
+		mask = cv2.imread('mask2.png', 0)
+		dst = cv2.inpaint(img, mask, 3, cv2.INPAINT_TELEA)
+		cv2.imshow('dst', dst)
+		cv2.waitKey(0)
+		cv2.destroyAllWindows()
 		print("")
 
 	def test_502(self):
